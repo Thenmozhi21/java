5c87fe6c11216f4e2d6b7565df0e357c505b33f7
thenmozhi velu <85513471+Thenmozhi21@users.noreply.github.com>
2022-04-07 12:01:41 +0530

    Add files via upload

---
+++ Pratical/OperatorExample.java
@@ -0,0 +1,12 @@
+class OperatorExample
+{
+public static void main(String args[])
+{
+int a=30;
+int b=20;
+System.out.println(a/b);
+System.out.println(a+b);
+System.out.println(a==b);
+System.out.println(a!=b);
+}
+}
