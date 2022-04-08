36d7dcdef424af5d4020eeb6eaf765e2c40590a9
thenmozhi velu <85513471+Thenmozhi21@users.noreply.github.com>
2022-04-08 13:36:42 +0530

    Add files via upload

---
+++ Pratical/ArrayExample.java
@@ -0,0 +1,17 @@
+class ArrayExample
+{
+public static void main(String args[])
+{
+String[]names={"honey","inba","pradeep","velu"};
+for (int i=0;i<names.length;i++)
+{
+System.out.println(names[i]);
+}
+System.out.println();
+int[]phones={123,123,135,678,9990};
+for(int i=0;i<phones.length;i++)
+{
+System.out.println(phones[i]);
+}
+}
+}
